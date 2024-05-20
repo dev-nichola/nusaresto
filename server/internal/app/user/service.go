12ad8386700f1f8e4ad6ee5c2b0b0db0acd18927
Efraim Munthe <efrayanglain@gmail.com>
2024-05-20 13:05:42 +0700

    change return type of find methods

--- server/internal/app/user/service.go
+++ server/internal/app/user/service.go
@@ -7,8 +7,8 @@ import (
 )
 
 type UserService interface {
-	FindAll(ctx context.Context) error
-	FindById(ctx context.Context, id uuid.UUID) error
+	FindAll(ctx context.Context) ([]User, error)
+	FindById(ctx context.Context, id uuid.UUID) (User, error)
 	Save(ctx context.Context) (User, error)
 	Update(ctx context.Context, id uuid.UUID) (User, error)
 	Delete(ctx context.Context, id uuid.UUID) error
@@ -22,12 +22,12 @@ func NewUserService(Repository UserRepository) UserService {
 	return &UserServiceImpl{Repository: Repository}
 }
 
-func (UserServiceImpl) FindAll(ctx context.Context) error {
-	return nil
+func (UserServiceImpl) FindAll(ctx context.Context) ([]User, error) {
+	return nil, nil
 }
 
-func (UserServiceImpl) FindById(ctx context.Context, id uuid.UUID) error {
-	return nil
+func (UserServiceImpl) FindById(ctx context.Context, id uuid.UUID) (User, error) {
+	return User{}, nil
 }
 
 func (UserServiceImpl) Save(ctx context.Context) (User, error) {
