4506d700e7969512adb6bf45784b6d9cc3a524e9
Efraim Munthe <efrayanglain@gmail.com>
2024-05-20 12:40:58 +0700

    create new service file

---
+++ server/internal/app/user/service.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	FindAll(ctx context.Context) error
+	FindById(ctx context.Context, id uuid.UUID) error
+	Save(ctx context.Context) (User, error)
+	Update(ctx context.Context, id uuid.UUID) (User, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type UserServiceImpl struct {
+	Repository UserRepository
+}
+
+func NewUserService(Repository UserRepository) UserService {
+	return &UserServiceImpl{Repository: Repository}
+}
+
+func (UserServiceImpl) FindAll(ctx context.Context) error {
+	return nil
+}
+
+func (UserServiceImpl) FindById(ctx context.Context, id uuid.UUID) error {
+	return nil
+}
+
+func (UserServiceImpl) Save(ctx context.Context) (User, error) {
+	return User{}, nil
+}
+
+func (UserServiceImpl) Update(ctx context.Context, id uuid.UUID) (User, error) {
+	return User{}, nil
+}
+
+func (UserServiceImpl) Delete(ctx context.Context, id uuid.UUID) error {
+	return nil
+}
